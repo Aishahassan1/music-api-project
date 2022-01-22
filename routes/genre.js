ba21861d370bb7ef0337d271af8ae83f2033636d
Aishahassan1 <aishahassan_5@hotmail.com>
2022-01-22 09:41:04 +0000

    update genre routes

--- routes/genre.js
+++ routes/genre.js
@@ -1,21 +1,19 @@
-
 const express = require('express');
+const passport = require('passport');
 const { Genre } = require('../models/genre');
-const passport = require('passport') 
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/genre', passport.authenticate('jwt', {session: false}), (req, res) => {
-  Genre.find({}, function (err, genre) {
-    if(err) {
+router.get('/genre', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Genre.find({}, (err, genre) => {
+    if (err) {
       res.status(404).json({
-        message: "genre not found"
+        message: 'genre not found',
       });
     } else {
-      res.json(genre)
+      res.json(genre);
     }
-  })
+  });
 });
-
 
 module.exports = router;
