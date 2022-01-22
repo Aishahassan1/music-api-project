9698ec9d747190be18f7d6527c07ac02ed941ffb
Aishahassan1 <aishahassan_5@hotmail.com>
2022-01-22 09:40:01 +0000

    update artist routes

--- routes/artist.js
+++ routes/artist.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const { Artist } = require('../models/artist')
-const passport = require('passport') 
+const passport = require('passport');
+const { Artist } = require('../models/artist');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/artist/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
-  Artist.findOne({ArtistId: parseInt(req.params.id)}, function (err, artist) {
-    if(err) {
+router.get('/artist/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Artist.findOne({ ArtistId: parseInt(req.params.id) }, (err, artist) => {
+    if (err) {
       res.status(404).json({
-        message: "artist not found"
+        message: 'artist not found',
       });
     } else {
-      res.json(artist)
+      res.json(artist);
     }
-  })
-})
-
+  });
+});
 
 module.exports = router;
