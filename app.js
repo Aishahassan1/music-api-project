eb2c8f9114a7d6fe33fc14fc436d46c983c7b157
Aishahassan1 <aishahassan_5@hotmail.com>
2022-01-22 09:52:25 +0000

    update app js code eslint

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport'); 
-const albumRouter = require('./routes/album');
+const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const albumRouter = require('./routes/album');
 const artistRouter = require('./routes/artist');
 const genreRouter = require('./routes/genre');
 const trackRouter = require('./routes/track');
-const userRouter = require('./routes/user'); 
+const userRouter = require('./routes/user');
 require('./middleware/auth');
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI);
 
 // const swaggerDocument = require('./swagger.json'); --> I KNOW I WILL NEED THIS FOR DOCUMENTATION BUT WILL I NEED THE OHTER ONE SINCE THERE WILL BE  NO USER INTERFACE?
 // const swaggerUi = require('swagger-ui-express'); --> not necessary unless you want a user interface for your documentation
@@ -19,25 +19,25 @@ require('./middleware/auth.js');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:false }));
-app.use('/api', userRouter); 
-app.use('/api', albumRouter); 
-app.use('/api', artistRouter); 
-app.use('/api', trackRouter); 
-app.use('/api', genreRouter); 
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', userRouter);
+app.use('/api', albumRouter);
+app.use('/api', artistRouter);
+app.use('/api', trackRouter);
+app.use('/api', genreRouter);
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.end()
+  res.end();
 });
 
 module.exports = app;
