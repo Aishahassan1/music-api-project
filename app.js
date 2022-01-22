d50831b37113f3e65827ffafaf81b4c274964752
Aishahassan1 <aishahassan_5@hotmail.com>
2022-01-22 08:35:23 +0000

    update eslint errors

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose')
-const passport = require('passport') 
-const albumRouter = require('./routes/album')
-const passportLocalMongoose = require('passport-local-mongoose') ;
+// const path = require('path');
+const mongoose = require('mongoose');
+// const passport = require('passport'); 
+const albumRouter = require('./routes/album');
+const passportLocalMongoose = require('passport-local-mongoose');
 const artistRouter = require('./routes/artist');
 const genreRouter = require('./routes/genre');
 const trackRouter = require('./routes/track');
-const userRouter =  require('./routes/user'); 
-require('./middleware/auth')
+const userRouter = require('./routes/user'); 
+require('./middleware/auth');
+
 mongoose.connect(process.env.MONGO_URI)
 
 // const swaggerDocument = require('./swagger.json'); --> I KNOW I WILL NEED THIS FOR DOCUMENTATION BUT WILL I NEED THE OHTER ONE SINCE THERE WILL BE  NO USER INTERFACE?
@@ -19,22 +20,18 @@ require('./middleware/auth.js');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended:false }));
 app.use('/api', userRouter); 
 app.use('/api', albumRouter); 
 app.use('/api', artistRouter); 
 app.use('/api', trackRouter); 
 app.use('/api', genreRouter); 
 
-
-
-
-
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,4 +42,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
